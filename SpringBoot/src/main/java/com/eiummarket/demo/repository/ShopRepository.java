46f9bbb4c86c10bdd475b6a0208b94db0c9341fc
conconcc <lesj9217@gmail.com>
2025-08-14 05:13:40 +0900

    feat : 시장 기본 CRUD 구현

---
+++ SpringBoot/src/main/java/com/eiummarket/demo/repository/ShopRepository.java
@@ -0,0 +1,15 @@
+package com.eiummarket.demo.repository;
+
+import com.eiummarket.demo.model.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    Optional<Shop> findByIdAndMarketId(Long shopId, Long marketId);
+
+    Page<Shop> findAllByMarketId(Long marketId, Pageable pageable);
+}
