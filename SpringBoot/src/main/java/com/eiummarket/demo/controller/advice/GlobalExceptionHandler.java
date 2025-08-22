bb2ce93950ab6cc68e9b9d6fb09ed86d21d7ebe3
Spooder <yoshi7591@naver.com>
2025-08-23 00:51:19 +0900

    fix: swagger 충돌 어노테이션 삭제

--- SpringBoot/src/main/java/com/eiummarket/demo/controller/advice/GlobalExceptionHandler.java
+++ SpringBoot/src/main/java/com/eiummarket/demo/controller/advice/GlobalExceptionHandler.java
@@ -6,12 +6,10 @@ import com.eiummarket.demo.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j // 로그 출력을 위해 추가
-@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // ResourceNotFoundException 처리
